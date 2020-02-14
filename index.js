910ab27c2052a779df2f53f453b37d701b4eb1a9
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-14 20:45:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9-']+/g, '')
   }
   static titleize(string){
-    let arr = ['a', 'an','and','but','of','for','at','by','from']
+    let arr = ['the','a', 'an','and','but','of','for','at','by','from']
     let a = string.split(" " )
     return a.map(ele => arr.includes(ele) ? ele : Formatter.capitalize(ele.toLowerCase())).join(' ')
 }
